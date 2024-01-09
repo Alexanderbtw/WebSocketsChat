77093f2c78fcaa5fa9cd3d19cc5010baaf23c687
Alexanderbtw <sgoldebaev@gmail.com>
2024-01-09 17:35:44 +0300

    Add project files.

---
+++ WebSocketsChat.Service/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using WebSocketsChat.Service.Models;
+
+namespace WebSocketsChat.Service.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task JoinChat(UserConnection conn)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined");
+        }
+
+        public async Task JoinSpecificChatRoom(UserConnection conn)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, conn.ChatRoom);
+            await Clients.Group(conn.ChatRoom).SendAsync("ReceiveMessage", "admin", $"{conn.Username} has joined the {conn.ChatRoom}");
+        }
+    }
+}
---
+++ web-sockets-chat-client-app/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wBzzyOUG2JBE9+wFiyLAuuLBVTeDEbg76YB3xzIRiDA=\"\n}"
