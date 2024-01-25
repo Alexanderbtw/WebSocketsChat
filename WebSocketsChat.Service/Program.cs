0ab7260bfbdc7fd6e1490e1fd78f1d165adeacf6
Alexanderbtw <sgoldebaev@gmail.com>
2024-01-25 15:55:40 +0300

    Added CORS

--- WebSocketsChat.Service/Program.cs
+++ WebSocketsChat.Service/Program.cs
@@ -3,10 +3,9 @@ using WebSocketsChat.Service.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddSignalR();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -34,9 +33,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
-app.MapControllers();
 app.MapHub<ChatHub>("/Chat");
 
 app.UseCors("reactApp");
